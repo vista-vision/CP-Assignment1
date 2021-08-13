cba252632b0f03637f3b5b8f5f2968c2f8d2c968
RAjALAkSHMi S <rajalakshmi.s@niser.ac.in>
2021-08-13 09:53:35 +0530

    Intro. Will never forget that silly string error.

---
+++ q1.py
@@ -0,0 +1,38 @@
+#                                             Sum of 1st N natural numbers and N odd numbers.
+
+# (for 5 different values of N)
+repeat=1
+while repeat!=6:
+
+    N=input("What natural number would you like to sum up to?")
+
+    try:
+        p=int(N)
+
+        if p<=0:
+            print("Enter a natural number!")
+
+        else:
+            #Sum of first N natural numbers.
+            n=1
+            i=1
+            while i!=p:
+                i+=1
+                n+=i
+            print(n)
+
+            #Sum of first N odd natural numbers.
+            i=1
+            m=1
+            j=1
+            while j!=p:
+                i+=2
+                m+=i
+                j+=1
+            print(m)
+
+            #To iterate for 5 values of N.
+            repeat+=1
+
+    except ValueError:
+        print("Enter a natural number!")
